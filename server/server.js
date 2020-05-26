ec6b0cc5063489d619c0f29522c076ba24c018a4
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-05-27 00:37:59 +0100

    Added Socket.io Connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
-app.listen(port, () =>
+
+io.on('connection', (socket) => {
+    console.log('New User Connected');
+
+    io.on("disconnection", (socket) => {
+        console.log("User was disconnected");
+    });
+});
+
+
+
+server.listen(port, () =>
     console.log(`Server is listening at http://localhost:${port}`)
 );
 module.exports = { app };
