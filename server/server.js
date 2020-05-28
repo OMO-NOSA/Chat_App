7ae89a28d8d974f9af813737aa4d771a6bf9a13a
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-05-28 20:57:51 +0100

    Added GeGreetings to all users, and also notify existing users that a new user is around

--- server/server.js
+++ server/server.js
@@ -14,7 +14,17 @@ app.use(express.static(publicPath));
 
 io.on("connection", (socket) => {
     console.log('New User Connected');
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to chat app',
+        createdAt: new Date().getTime()
+    });
 
+    socket.broadcast.emit("newMessage", {
+        from: 'Admin',
+        text: 'Someone New is here',
+        createdAt: new Date().getTime()
+    });
 
     socket.on("createMessage", (message) => {
         console.log("createMessage", message);
@@ -24,6 +34,12 @@ io.on("connection", (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit("newMessage", {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnection', () => {
