22d5d531e6089f8bfb30110ec6247f65c861d0c7
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-06-06 20:26:51 +0100

    Modified some files

--- public/js/index.js
+++ public/js/index.js
@@ -13,7 +13,7 @@ socket.on("disconnect", function() {
 socket.on("newMessage", function(message) {
     console.log('New Message', message);
     let li = jQuery('<li></li>');
-    li.text(`$(from): $(message.text)`);
+    li.text(`${message.from}: ${message.text}`);
 
     jQuery('#messages').append(li);
 });
