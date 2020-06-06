d47f10b798b71c5f9c8d36a25674bf5236c3bf6d
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-06-06 20:17:06 +0100

    Added JQuery for document manipulation

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,10 @@ socket.on("disconnect", function() {
 
 socket.on("newMessage", function(message) {
     console.log('New Message', message);
+    let li = jQuery('<li></li>');
+    li.text(`$(from): $(message.text)`);
+
+    jQuery('#messages').append(li);
 });
 
 
@@ -20,4 +24,15 @@ socket.emit('createMessage', {
     text: 'Hi'
 }, function() {
     console.log('Got it!')
+});
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ io.on("connection", (socket) => {
 
     socket.broadcast.emit("newMessage", ("Admin", "Someone new is here, say a big welcome"));
 
-    socket.on("createMessage", (message) => {
+    socket.on("createMessage", (message, callback) => {
         console.log("createMessage", message);
 
         io.emit('newMessage', generateMessage('message.from', 'message.text'));
-        callback();
+        callback('This is from the server!!!');
         // socket.broadcast.emit("newMessage", {
         //     from: message.from,
         //     text: message.text,
