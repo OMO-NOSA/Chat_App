2cf76b82d8fdc4098ca29e10c4ebfcf46f3fc565
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-07-13 15:22:20 +0100

    Reset Jquery input field

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,12 @@ socket.emit('createMessage', {
 
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
-
     socket.emit('createMessage', {
         from: 'User',
         text: jQuery('[name=message]').val()
     }, function() {
 
     });
+
+    this.reset();
 });
