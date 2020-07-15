f79ccb6b18fe25ba0abd47f2d890bddc884701f5
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-07-15 06:00:52 +0100

    Added momentJs

---
+++ playground/time.js
@@ -0,0 +1,6 @@
+const moment = require("moment");
+//import * as moment from 'moment';
+let date = new Date();
+
+console.log(date.getMonth());
+console.log(moment().format('MMMM Do YYYY H:mm:ss A'));
