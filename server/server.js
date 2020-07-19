9fcb5c9fefec82d0c5d8fa1ae69e6ac5c5469e7b
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-07-19 09:41:23 +0100

    Added updateUserList event

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ const io = socketIO(server);
 const port = process.env.PORT || 3000;
 const { generateMessage, generateLocationMessage } = require("./utils/message");
 const { isRealString } = require("./utils/validation");
+const { Users } = require("./utils/users");
+const users = new Users();
+
+
 
 app.use(express.static(publicPath));
 
@@ -19,11 +23,15 @@ io.on("connection", (socket) => {
 
     socket.on('join', (params, callback) => {
         if (!isRealString(params.name) || !isRealString(params.room)) {
-            callback('Name and room name are required.');
+            return callback('Name and room name are required.');
         }
         socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
 
 
+
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room))
         socket.emit(
             "newMessage",
             generateMessage("Admin", "Welcome to the Chat App")
@@ -31,7 +39,7 @@ io.on("connection", (socket) => {
 
         socket.broadcast.to(params.room).emit(
             "newMessage",
-            ("Admin", `${params.name} is here, say a big welcome`)
+            generateMessage("Admin", `${params.name} is here, say a big welcome`)
         );
         callback();
     });
@@ -51,8 +59,15 @@ io.on("connection", (socket) => {
         io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
-    socket.on('disconnection', () => {
+    socket.on('disconnect', () => {
         console.log('User was disconnected');
+        let user = users.removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `User has ${user.name} has left`));
+        }
+
     });
 });
 
